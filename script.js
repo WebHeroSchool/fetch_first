8e540bbea0e340d90f90606fc27d8cb73b023d08
Anastasia Miheeva <mikheevaanastasiia93@yandex.ru>
2020-02-18 14:08:35 +0300

    corrections

--- script.js
+++ script.js
@@ -3,7 +3,6 @@ let body = document.body;
 let preloader = document.getElementById('page-preloader')
 let newDate = new Date();
 
-
 let getUserName = (url) => {
 	let urlArr = url.split('=');
   let userName = urlArr[1];
@@ -13,11 +12,13 @@ let getUserName = (url) => {
   return userName;
 }
 
+let user = getUserName(url)
+
 let getNewDate = new Promise((resolve, reject) => {
   setTimeout(() => newDate ? resolve(newDate) : reject('Время не определено'), 2000)
 })
  
-let userInfo = fetch(`https://api.github.com/users/${getUserName(url)}`)
+let userInfo = fetch(`https://api.github.com/users/${user}`)
 
 Promise.all([userInfo, getNewDate])
 .then(([infoData, todayDate]) => {
@@ -30,45 +31,50 @@ Promise.all([userInfo, getNewDate])
   let userLogin = json.login;
   let userBio = json.bio;
   let userUrl = json.html_url;
+  if (user) {
+    let getUserLogin = () => {
+      let createLoginEl = document.createElement('h1');
+      createLoginEl.innerHTML = userLogin;
+      body.appendChild(createLoginEl);
+    }
+
+    let getUserAvatar = () => {
+      let createImgEl = document.createElement('img');
+      createImgEl.src = userAvatar;
+      body.appendChild(createImgEl);
+    }
+
+    let getUserBio = () => {
+      let createBioEl = document.createElement('p');
+      createBioEl.innerHTML = userBio;
+      body.appendChild(createBioEl)
+    }
+
+    let getUserUrl = () => {
+      let createUrlEl = document.createElement('a');
+      let linkText = document.createTextNode('Профиль');
+      createUrlEl.href = userUrl;
+      createUrlEl.appendChild(linkText);
+      body.appendChild(createUrlEl)
+    }
+
+    let getTodayDate = () => {
+      let createDateEl = document.createElement('p');
+      createDateEl.innerHTML = receiveTodayDate;
+      body.appendChild(createDateEl);
+    }
+
+    preloader.classList.toggle('load')
+    getUserLogin();
+    getUserAvatar();
+    getUserBio();
+    getUserUrl()
+    getTodayDate()
+  } 
+  else {
+    alert('Пользователь не найден')
+  }  
   
-  let getUserLogin = () => {
-    let createLoginEl = document.createElement('h1');
-    createLoginEl.innerHTML = userLogin;
-    body.appendChild(createLoginEl);
-  }
-
-  let getUserAvatar = () => {
-    let createImgEl = document.createElement('img');
-    createImgEl.src = userAvatar;
-    body.appendChild(createImgEl);
-  }
-
-  let getUserBio = () => {
-    let createBioEl = document.createElement('p');
-    createBioEl.innerHTML = userBio;
-    body.appendChild(createBioEl)
-  }
-
-  let getUserUrl = () => {
-    let createUrlEl = document.createElement('a');
-    let linkText = document.createTextNode('Профиль');
-    createUrlEl.href = userUrl;
-    createUrlEl.appendChild(linkText);
-    body.appendChild(createUrlEl)
-  }
-
-  let getTodayDate = () => {
-    let createDateEl = document.createElement('p');
-    createDateEl.innerHTML = receiveTodayDate;
-    body.appendChild(createDateEl);
-  }
-
-  preloader.classList.toggle('load')
-  getUserLogin();
-  getUserAvatar();
-  getUserBio();
-  getUserUrl()
-  getTodayDate()
 })
 
 .catch(err => alert(`${err} (Информация о пользователе не доступна)`))
